60cc30d4f5275427b04bbc889653cd0056c55a6c
Jorge Garcia <jlg.sftdev@gmail.com>
2019-09-24 20:11:24 -0700

    Completed the 'this' exercise

--- scope-exercises/hoisting/ex.js
+++ scope-exercises/hoisting/ex.js
@@ -1,8 +1,3 @@
-/*
-Refactor all inline function expressions to be function declarations. Place function declarations at the bottom (that is, below any executable code) of their respective scopes.
-
-Also, pull function declarations to outer scopes if they don't need to be nested.
-*/
 function getStudentFromId(studentId) {
   return studentRecords.find(matchId);
 
